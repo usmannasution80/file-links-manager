b0180f139f6eef2293923ce437230262af714ee2
Usman Nasution <usmannasty32@gmail.com>
2023-08-12 13:22:54 +0700

    Created main.py and app.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+  title = 'FILE LINKS MANAGER'
+  sep_len = len(title)
+  print(
+    ('=' * sep_len),
+    title,
+    ('=' * sep_len),
+    '1. List',
+    '2. Insert',
+    '3. Exit',
+    ('=' * sep_len),
+    'Your choice : ',
+    sep = '\n'
+  )
+
+main();
